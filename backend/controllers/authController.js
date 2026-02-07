2770b280dac3c2600bef02d29042906c5a5c7b5d
ola <hollabodey.kilaso@gmail.com>
2026-02-07 20:54:51 +0000

    feat: implement login functionality and user profile retrieval

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -57,6 +57,30 @@ const registerUser = async (req, res) => {
 // @access Public
 const loginUser = async (req, res) => {
     try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid email or password" });
+        }
+
+        // Check password
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+
+        // Return user data and token
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            profileImageUrl: user.profileImageUrl,
+            token: generateToken(user._id)
+        });
+
     } catch (error) {
         res.status(500).json({ message: "Server error", error: error.message });
     }
@@ -67,6 +91,13 @@ const loginUser = async (req, res) => {
 // @access Private
 const getUserProfile = async (req, res) => {
     try {
+        const user = await User.findById(req.user._id).select("-password");
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.json(user);
+        
     } catch (error) {
         res.status(500).json({ message: "Server error", error: error.message });
     }
