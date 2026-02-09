3972875a4ede57cc1a66ac2f53224cc19fc3c800
ola <hollabodey.kilaso@gmail.com>
2026-02-09 17:33:13 +0000

    feat: implement user management routes and task counting in user retrieval

---
+++ backend/controllers/userController.js
@@ -0,0 +1,76 @@
+const Task = require("../models/Task");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+
+// @desc Get all users (admin only)
+// @route GET /api/users
+// @access Private/Admin
+const getUsers = async (req, res) => {
+    try {
+        const users = await User.find({ role: "student" }).select("-password");
+
+        const usersWithTaskCounts = await Promise.all(users.map(async (user) => {
+            const pendingTasks = await Task.countDocuments({
+                assignedTo: user._id,
+                status: "pending"
+            });
+
+            const inProgressTasks = await Task.countDocuments({
+                assignedTo: user._id,
+                status: "in-progress"
+            })
+
+            const completedTasks = await Task.countDocuments({
+                assignedTo: user._id,
+                status: "completed"
+
+            })
+
+            return {
+                ...user._doc, // Include all existing user data
+                pendingTasks,
+                inProgressTasks,
+                completedTasks,
+            };
+        }));
+
+        res.json(usersWithTaskCounts);
+
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+}
+
+// @desc Get user by ID (admin only)
+// @route GET /api/users/:id
+// @access Private
+const getUserById = async (req, res) => {
+    try {
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+}
+
+
+// @desc Delete user (admin only)
+// @route DELETE /api/users/:id
+// @access Private/Admin
+const deleteUser = async (req, res) => {
+    try {
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+}
+
+
+// const getInstructor = async (req, res) => {
+//     try{
+//         const users = await User.find({ role: "instructor" }).select("-password");
+//         res.json(users);
+//     } catch (error) {
+//         res.status(500).json({ message: "Server error", error: error.message });
+//     }
+// }
+
+module.exports = { getUsers, getUserById, deleteUser };
--- backend/models/Task.js
+++ backend/models/Task.js
@@ -3,14 +3,21 @@ const { createIndexes } = require("./User");
 
 const todoSchema = new mongoose.Schema(
     {
+        text: { type: String, required: true },
+        completed: { type: Boolean, default: false }
+    }
+);
+
+const taskSchema = new mongoose.Schema(
+    {
         title: { type: String, required: true },
         description: { type: String },
         priority: { type: String, enum: ['low', 'medium', 'high'], default: 'medium' },
         status: { type: String, enum: ['pending', 'in-progress', 'completed'], default: 'pending' },
         dueDate: { type: Date, required: true },
-        
+
         assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-        createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+        createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
         attachments: [{ type: String }],
         todoChecklist: [todoSchema],
         progress: { type: Number, default: 0 },
@@ -19,4 +26,4 @@ const todoSchema = new mongoose.Schema(
 )
 
 
-module.exports = mongoose.model("Task", taskSchema);
+module.exports = mongoose.model("Task", todoSchema);
