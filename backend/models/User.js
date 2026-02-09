262a696ddbba73b274f78ec297fc9f224f52bf72
ola <hollabodey.kilaso@gmail.com>
2026-02-09 15:10:09 +0000

    feat: add profileImageUrl field to user schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema(
         name: { type: String, required: true},
         email: {type: String, required: true, unique: true},
         password: {type: String, required: true},
+        profileImageUrl: { type: String, default: null},
         role: {type: String, enum: ['student', 'instructor'], default: "student"},
 
         createdAt: { type: Date, default: Date.now },
