c64431e3bd464e810543307a350fc46025d0bf05
ola <hollabodey.kilaso@gmail.com>
2026-02-10 20:00:46 +0000

    feat: enhance Signup component with validation and error handling

--- frontend/instructor-booking/src/pages/Auth/Login.jsx
+++ frontend/instructor-booking/src/pages/Auth/Login.jsx
@@ -29,8 +29,6 @@ const Login = () => {
 
     // Login API call
 
-
-
   };
 
   return (
--- frontend/instructor-booking/src/pages/Auth/Signup.jsx
+++ frontend/instructor-booking/src/pages/Auth/Signup.jsx
@@ -1,25 +1,55 @@
-import React, { useState } from 'react'
-import AuthLayout from '../../components/layouts/AuthLayout'
+import React, { useState } from "react";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import { validateEmail } from "../../utils/helper";
 
 const Signup = () => {
   const [profilePic, setProfilePic] = useState(null);
-  const [fullName, setFullName] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [adminInviteToken, setAdminInviteToken] = useState("")
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [adminInviteToken, setAdminInviteToken] = useState("");
 
   const [error, setError] = useState(null);
-  
+
+  // Handle signup  form submit
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    if (!fullName) {
+      setError("Please enter fullname");
+      return;
+    }
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter the password");
+      return;
+    }
+
+    setError("");
+
+    // signup API call
+  };
+
   return (
     <AuthLayout>
-      <div className='lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center'>
-        <h3 className='text-xl font-semibold text-black'>Create an Account</h3>
-        <p className='text-xs text-slate-700 mt-1.25 mb-6'>
+      <div className="lg:w-full h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Create an Account</h3>
+        <p className="text-xs text-slate-700 mt-1.25 mb-6">
           Join us today by entering your details below.
         </p>
+
+        <form onSubmit={handleSignUp}>
+          <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4"></div>
+        </form>
       </div>
     </AuthLayout>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
