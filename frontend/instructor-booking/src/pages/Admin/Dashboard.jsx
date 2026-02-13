eef6ceabb2dc8e587c2e1c9717f06f23b2e0a11c
ola <hollabodey.kilaso@gmail.com>
2026-02-13 20:53:11 +0000

    feat: enhance DashboardLayout to include user context and conditional rendering of SideMenu

--- frontend/instructor-booking/src/pages/Admin/Dashboard.jsx
+++ frontend/instructor-booking/src/pages/Admin/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { useUserAuth } from '../../hooks/useUserAuth'
 import { UserContext } from '../../context/userContext';
+import DashboardLayout from '../../components/layouts/DashboardLayout';
 
 const Dashboard = () => {
   useUserAuth();
