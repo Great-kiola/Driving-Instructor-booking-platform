e59367d9e34631bdd7e57fe80ceb2c1186c8bfb0
ola <hollabodey.kilaso@gmail.com>
2026-02-16 18:54:55 +0000

    feat: implement Sidemenu component with dynamic menu items based on user role

--- frontend/instructor-booking/src/components/layouts/Sidemenu.jsx
+++ frontend/instructor-booking/src/components/layouts/Sidemenu.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { UserContext } from '../../context/userContext'
+import { SIDE_MENU_DATA, SIDE_MENU_DATA_USER } from '../../utils/data';
+import { useNavigate } from "react-router-dom";
 
-const Sidemenu = () => {
-  return (
-    <div>Sidemenu</div>
-  )
+const Sidemenu = ({activeMenu}) => {
+
+  const {user, clearUser } = useContext(UserContext);
+  const [sideMenuData, setSideMenuData] =  useState([]);
+
+  const navigate = useNavigate();
+
+  const handleClick = (route) => {
+    if(route === "logout"){
+      handleLogout();
+      return;
+    }
+    navigate(route)
+  };
+
+  const handleLogout = () => {
+    localStorage.clear();
+    clearUser();
+    navigate("/login");
+  }
+
+  useEffect(() => {
+    if(user){
+      setSideMenuData(user?.role === 'admin' ? SIDE_MENU_DATA : SIDE_MENU_DATA_USER)
+    }
+
+    return () => {};
+  }, [user]);
+
+  return <div className=''>
+    <div className=''>
+      <div className=''>
+        <img 
+          src={user?.profileImageUrl || ""}
+          alt="Profile Image"
+          className=''
+        />
+      </div>
+
+      {user?.role === "admin" && (
+        <div className=''>
+          Instructor
+        </div>
+      )}
+
+      <h5 className=''>
+        {user?.name || ""}
+      </h5>
+
+      <p className=''> {user?.email || ""} </p>
+    </div>
+
+      {sideMenuData.map((item, index) => {
+        <button
+          key={`menu_${index}`}
+          className={`w-full flex items-center gap-4 text-[15px] ${
+            activeMenu == item.label
+            ? "text-primary bg-linear-to-r from-blue-50/40 to blue-100/50 border-r-3"
+            : ""
+          } py-3 px-6 mb-3 cursor-pointer`}
+          onClick={() => handleClick(item.path)}
+        >
+          <item.icon className="" />
+          {item.label}
+        </button>
+      })};
+    </div>
+  
 }
 
 export default Sidemenu
