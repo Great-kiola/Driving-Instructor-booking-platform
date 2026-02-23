a99d3280c707d71be440457b256e2184db209173
ola <hollabodey.kilaso@gmail.com>
2026-02-23 06:31:03 +0000

    feat: implement Modal component and integrate it into SelectUsers for user selection

--- frontend/instructor-booking/src/components/inputs/SelectUsers.jsx
+++ frontend/instructor-booking/src/components/inputs/SelectUsers.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axiosInstance from "../../utils/axiosInstance";
 import { API_PATHS } from "../../utils/apiPaths";
 import { LuUsers } from "react-icons/lu";
+import Modal from "../Modal";
 
 const SelectUsers = ({ selectedUsers, setSelectedUsers }) => {
   const [allUsers, setAllUsers] = useState([]);
@@ -55,6 +56,40 @@ const SelectUsers = ({ selectedUsers, setSelectedUsers }) => {
           <LuUsers className="text-sm" /> Add Members
         </button>
       )}
+
+      <Modal
+        isOpen={isModalOpen}
+        onClose={()=> setIsModalOpen(false)}
+        title="Select Users"
+      >
+        <div className="space-y-4 h-60[vh] overflow-y-auto">
+          {allUsers.map((user) => (
+            <div
+              key={user._id}
+              className="flex items-center gap-4 p-3 border-b border-gray-200"
+            >
+              <img
+                src={user.profileImageUrl}
+                alt={user.name}
+                className="w-10 h-10 rounded-full"
+              />
+
+              <div className="flex-1">
+                <p className="font-medium text-gray-800 dark:text-white">
+                  {user.name}
+                </p>
+                <p className="">{user.email}</p>
+              </div>
+
+              <input
+                type="checkbox"
+                checked={tempSelectedUsers.includes(user._id)}
+                onChange={() => toggleUserSelection(user._id)}
+              />
+            </div>
+          ))}
+        </div>
+      </Modal>
     </div>
   );
 };
