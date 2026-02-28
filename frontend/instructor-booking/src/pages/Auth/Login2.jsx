f244a6ff17329962830af8f7d79515a21dc30df4
ola <hollabodey.kilaso@gmail.com>
2026-02-28 01:46:30 +0000

    feat: add Input2 component and integrate it into Login2 page; enhance form structure and styling

--- frontend/instructor-booking/src/pages/Auth/Login2.jsx
+++ frontend/instructor-booking/src/pages/Auth/Login2.jsx
@@ -1,14 +1,14 @@
 import React from "react";
+import Input2 from "../../components/inputs/Input2";
 
 const Login2 = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="">
-        <h1 className="text-xl">Welcome Back!</h1>
+    <div className="flex items-center justify-center h-screen bg-[#f5f5f6]">
+      <div className="bg-[#fefffe] p-20 rounded-lg border border-[#e5e5e6]">
+        <h1 className="text-2xl font-semibold">Welcome Back!</h1>
 
         <form>
-        <label>Email Address</label>
-          <input
+          <Input2
             type="text"
             // value={email}
             // onChange={({ target }) => setEmail(target.value)}
@@ -16,11 +16,10 @@ const Login2 = () => {
             label="Email Address"
           />
 
-          <input 
+          <Input2
             type="password"
             placeholder="****"
             label="Password"
-            className="border p-1.5"
           />
 
         </form>
