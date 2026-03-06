89f40a07ace04cb6a665b44ba16dfa682d8f9a8b
ola <hollabodey.kilaso@gmail.com>
2026-03-06 00:18:04 +0000

    fix: remove unused index parameter from allTasks map function in MyTasks component

--- frontend/instructor-booking/src/pages/User/MyTasks.jsx
+++ frontend/instructor-booking/src/pages/User/MyTasks.jsx
@@ -86,7 +86,7 @@ const MyTasks = () => {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
-          {allTasks?.map((item, index) => (
+          {allTasks?.map((item) => (
             <TaskCard
               key={item._id}
               title={item.title}
