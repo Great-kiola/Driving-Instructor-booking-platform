9a37967fbf02425afc093cfe51a6899a7c650c00
ola <hollabodey.kilaso@gmail.com>
2026-03-18 18:18:34 +0000

    Refactor code structure for improved readability and maintainability

--- frontend/instructor-booking/src/pages/User/SearchInstructors.jsx
+++ frontend/instructor-booking/src/pages/User/SearchInstructors.jsx
@@ -1,26 +1,36 @@
 import React from "react";
 import DashboardLayout from "../../components/layouts/DashboardLayout";
-
+import { LuSearch } from "react-icons/lu";
 
 const SearchInstructors = () => {
+  const handleClick = () => {
+    alert("Button Clicked!");
+  };
   return (
     <DashboardLayout activeMenu="Search Instructors">
       <div className="card2 my-5 ">
-        <h2 className="text-3xl text-center"> Enter your location</h2>
+        <h2 className="text-3xl text-center"> Enter your location </h2>
 
-        <div className="search-bar bg-[#edf0f2] p-5 rounded-xl w-full mt-8">
-          <input
-            type="text"
-            placeholder="Enter your location"
-            className="border-2 w-full p-5" 
+        <div className="search-bar bg-[#edf0f2] rounded-full w-full mt-8">
+          <form
+            onSubmit={handleClick}
+            className="flex justify-between items-center"
           >
+            <input
+              type="text"
+              placeholder="Enter your location"
+              className="w-full p-5 focus:outline-none ml-5 text-lg"
+            ></input>
 
-          </input>
+            <button className="w-55 flex items-center justify-center bg-primary p-5 rounded-full m-2 text-white hover:bg-blue-700">
+              <LuSearch className="text-xl mr-2"/>
+              Search
+            </button>
+          </form>
+
+
         </div>
-
-
       </div>
-
     </DashboardLayout>
   );
 };
