5163eeeb95eaf8a3db96b7d110bb897b86a4ec96
ola <hollabodey.kilaso@gmail.com>
2026-03-18 14:29:19 +0000

    feat: add card2 styling for SearchInstructors component

--- frontend/instructor-booking/src/pages/User/SearchInstructors.jsx
+++ frontend/instructor-booking/src/pages/User/SearchInstructors.jsx
@@ -5,11 +5,22 @@ import DashboardLayout from "../../components/layouts/DashboardLayout";
 const SearchInstructors = () => {
   return (
     <DashboardLayout activeMenu="Search Instructors">
-      <div>
-        <div className="col-span-3">
+      <div className="card2 my-5 ">
+        <h2 className="text-3xl text-center font-bold"> Find Instructors Near you</h2>
 
+        <div className="search-bar bg-amber-300 p-5 rounded-xl w-full">
+          <input
+            type="text"
+            placeholder="Search Here"
+            className="border-2 w-full p-5" 
+          >
+
+          </input>
         </div>
+
+
       </div>
+
     </DashboardLayout>
   );
 };
