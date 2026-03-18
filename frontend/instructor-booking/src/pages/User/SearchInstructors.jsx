d64ea2b86197d9be84196628e2254ad8e511fa91
ola <hollabodey.kilaso@gmail.com>
2026-03-18 12:41:44 +0000

    refactor: improve SearchInstructors component structure and formatting

--- frontend/instructor-booking/src/pages/User/SearchInstructors.jsx
+++ frontend/instructor-booking/src/pages/User/SearchInstructors.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import DashboardLayout from '../../components/layouts/DashboardLayout'
+import React from "react";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+
 
 const SearchInstructors = () => {
   return (
-    <DashboardLayout activeMenu="Search Instructors">SearchInstructors</DashboardLayout>
-  )
-}
+    <DashboardLayout activeMenu="Search Instructors">
+      <div>
+        <div className="col-span-3">
 
-export default SearchInstructors
+        </div>
+      </div>
+    </DashboardLayout>
+  );
+};
+
+export default SearchInstructors;
