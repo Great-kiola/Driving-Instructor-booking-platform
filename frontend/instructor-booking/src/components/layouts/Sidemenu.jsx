37f8f96914bf732e89845ef13ada3924bdbe4d06
ola <hollabodey.kilaso@gmail.com>
2026-04-19 15:13:43 +0100

    fix: add padding to SideMenu component for improved layout

--- frontend/instructor-booking/src/components/layouts/Sidemenu.jsx
+++ frontend/instructor-booking/src/components/layouts/Sidemenu.jsx
@@ -34,7 +34,7 @@ const SideMenu = ({ activeMenu }) => {
   }, [user]);
 
   return (
-    <div className="w-64 h-[calc(100vh-61px)] bg-white border-r border-gray-200/50 sticky top-15.25 z-20 ">
+    <div className="w-64 h-[calc(100vh-61px)] bg-white border-r border-gray-200/50 sticky top-15.25 z-20 p-5">
       <div className="flex flex-col items-center justify-center mb-7 pt-5">
         <div className="relative">
           <img
@@ -60,7 +60,7 @@ const SideMenu = ({ activeMenu }) => {
       {sideMenuData.map((item, index) => (
         <button
           key={`menu_${index}`}
-          className={`w-full flex items-center gap-4 text-[14px] ${
+          className={`w-full flex items-center gap-4 text-[14px] hover:bg-primary hover:rounded-3xl hover:text-white ${
             activeMenu == item.label
               ? "text-primary bg-linear-to-r from-blue-50/40 to blue-100/50 border-r-3"
               : ""
